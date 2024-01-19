7f6891df23df4342fefc42f119059a2b5a518b42
sangatification <rokayasangat123@gmail.com>
2024-01-19 14:04:35 +0545

    footer and navbar created

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -1,55 +1,9 @@
-'use client'
-import React from 'react';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+import React from 'react'
 
-const SignupSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  lastName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
-});
+const page = () => {
+  return (
+    <div>Register</div>
+  )
+}
 
-const ValidationSchemaExample = () => (
-  <div>
-    <h1>Signup</h1>
-    <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={values => {
-        // same shape as initial values
-        console.log(values);
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          <Field name="firstName" placeholder="enter first name"/>
-          {errors.firstName && touched.firstName ? (
-            <div>{errors.firstName}</div>
-          ) : null}
-          <br/>
-          <Field name="lastName" placeholder="enter last name"/>
-          {errors.lastName && touched.lastName ? (
-            <div>{errors.lastName}</div>
-          ) : null}
-          <br/>
-          <Field name="email" type="email" placeholder="enter email"/>
-          {errors.email && touched.email ? <div>{errors.email}</div> : null}
-          <br/>
-          <button type="submit">Submit</button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
-
-export default ValidationSchemaExample
+export default page
---
+++ client/src/components/navBar/page.js
@@ -0,0 +1,69 @@
+'use client'
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar, Button } from "@nextui-org/react";
+import React from 'react'
+import Image from "next/image";
+
+const page = () => {
+    return (
+        <Navbar isBordered>
+            <NavbarContent justify="start">
+                <NavbarBrand className="mr-4">
+                    {/* <AcmeLogo /> */}
+                    <Image src = "/parcellogo.png" width= "70" height = "70"/>
+                    <p className="hidden sm:block font-bold text-inherit">KHA! SAMAN</p>
+                </NavbarBrand>
+                <NavbarContent className="hidden sm:flex gap-3">
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            Features
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem isActive>
+                        <Link href="#" aria-current="page" color="secondary">
+                            Customers
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            Integrations
+                        </Link>
+                    </NavbarItem>
+                </NavbarContent>
+            </NavbarContent>
+
+            <NavbarContent as="div" className="items-center" justify="end">
+                <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">Login</Button>
+                <Link href="/register" color="success">Register</Link>
+                <Dropdown placement="bottom-end">
+                    <DropdownTrigger>
+                        <Avatar
+                            isBordered
+                            as="button"
+                            className="transition-transform"
+                            name="Profile"
+                            showFallback
+                            src="https://images.unsplash.com/broken"
+                        />
+                    </DropdownTrigger>
+                    <DropdownMenu aria-label="Profile Actions" variant="flat">
+                        <DropdownItem key="profile" className="h-14 gap-2">
+                            <p className="font-semibold">Signed in as</p>
+                            <p className="font-semibold">zoey@example.com</p>
+                        </DropdownItem>
+                        <DropdownItem key="settings">My Settings</DropdownItem>
+                        <DropdownItem key="team_settings">Team Settings</DropdownItem>
+                        <DropdownItem key="analytics">Analytics</DropdownItem>
+                        <DropdownItem key="system">System</DropdownItem>
+                        <DropdownItem key="configurations">Configurations</DropdownItem>
+                        <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+                        <DropdownItem key="logout" color="danger">
+                            Log Out
+                        </DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+            </NavbarContent>
+        </Navbar>
+    )
+}
+
+export default page
