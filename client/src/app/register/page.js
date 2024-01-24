e4fe49a85836f90a6c245817a7f6aec365100706
sangatification <rokayasangat123@gmail.com>
2024-01-24 19:57:41 +0545

    added alert UI

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -4,6 +4,8 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { Button, Input } from "@nextui-org/react";
 import styles from './styles.module.css'
+import { toast } from 'react-toastify';
+
 
 
 
@@ -26,14 +28,14 @@ const SignupForm = () => {
       .required('required')
   });
 
-  const registerUser = async(values)=> {
+  const registerUser = async (values) => {
     const res = await fetch('http://localhost:4000/register', {
       method: 'POST',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(values)
     })
     const data = await res.json()
-    alert(data.msg)
+    toast(data.msg)
   }
 
   const formik = useFormik({
@@ -59,74 +61,74 @@ const SignupForm = () => {
         <p className={styles.disclaimar}>Remember, only legit credentials can get you registered.</p>
         <p className={styles.disclaimar}>No invisible ink or secret handshakes, please! 🦸‍♀️💻</p>
       </label>
-      <br/>
-        <label htmlFor="fullName">Full Name</label>
-        <Input
-          id="fullName"
-          name="fullName"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.fullName}
-          placeholder='Enter your full name' />
-        {formik?.errors.fullName}
+      <br />
+      <label htmlFor="fullName">Full Name</label>
+      <Input
+        id="fullName"
+        name="fullName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.fullName}
+        placeholder='Enter your full name' />
+      {formik?.errors.fullName}
 
-        <br />
+      <br />
 
-        <label htmlFor="email">Email</label>
-        <Input
-          id="email"
-          name="email"
-          type="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-          placeholder='Enter your email' />
-        {formik?.errors.email}
-
-
-        <br />
+      <label htmlFor="email">Email</label>
+      <Input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        placeholder='Enter your email' />
+      {formik?.errors.email}
 
 
-        <label htmlFor="phoneNumber">Phone Number</label>
-        <Input
-          id="phoneNumber"
-          name="phoneNumber"
-          type="phoneNumber"
-          onChange={formik.handleChange}
-          value={formik.values.phoneNumber}
-          placeholder='Enter your phone number' />
-        {formik?.errors.phoneNumber}
+      <br />
 
 
-        <br />
+      <label htmlFor="phoneNumber">Phone Number</label>
+      <Input
+        id="phoneNumber"
+        name="phoneNumber"
+        type="phoneNumber"
+        onChange={formik.handleChange}
+        value={formik.values.phoneNumber}
+        placeholder='Enter your phone number' />
+      {formik?.errors.phoneNumber}
 
 
-        <label htmlFor="password">password</label>
-        <Input
-          id="password"
-          name="password"
-          type="password"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-          placeholder='Enter your password' />
-        {formik?.errors.password}
-
-        <br/>
+      <br />
 
 
-        <label htmlFor="role">Role</label>
-        <Input
-          id="role"
-          name="role"
-          type="role"
-          onChange={formik.handleChange}
-          value={formik.values.role}
-          placeholder='Enter your role' />
-        {formik?.errors.role}
+      <label htmlFor="password">password</label>
+      <Input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        placeholder='Enter your password' />
+      {formik?.errors.password}
+
+      <br />
 
 
-        <br />
+      <label htmlFor="role">Role</label>
+      <Input
+        id="role"
+        name="role"
+        type="role"
+        onChange={formik.handleChange}
+        value={formik.values.role}
+        placeholder='Enter your role' />
+      {formik?.errors.role}
 
-        <Button className='registerBotton' type="submit">Submit</Button>
+
+      <br />
+
+      <Button className='registerBotton' type="submit">Submit</Button>
     </form>
   );
 };
--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,19 +1,19 @@
 const User = require('../models/user')
-const registerNewUser = async(req, res)=> {
-    try{
-        const userDetail = await User.findOne({phoneNumber: req.body.phoneNumber})
-        if(userDetail){
-            res.status(403).json({msg: "Phone number already registered"})
-        }else{
-            User.create(req.body)
+const registerNewUser = async (req, res) => {
+    try {
+        const userDetail = await User.findOne({ phoneNumber: req.body.phoneNumber })
+        if (userDetail) {
+            res.status(403).json({ msg: "Phone number already registered" })
+        } else {
+            await User.create(req.body)
             res.json({
                 msg: "User registered successfully"
             })
         }
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 
 }
 
-module.exports = {registerNewUser}
+module.exports = { registerNewUser }
